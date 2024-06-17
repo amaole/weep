2509da41842d69f96a8cab9acaa7a6c2377758cb
马璟 <2443065482@qq.com>
2024-06-17 11:20:27 +0800

    Updates

--- js/detail-C_zKLCAn.js
+++ js/detail-C_zKLCAn.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("详情");const a=JSON.parse(localStorage.getItem("id")),i=JSON.parse(localStorage.getItem("index")),s=a.slice(JSON.stringify(i),1);console.log(s);e(".box").innerHTML=`
+import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("详情");const n=JSON.parse(localStorage.getItem("id")),o=JSON.parse(localStorage.getItem("index"))*1;console.log(n,o);const s=n.filter(a=>a.item_id===o);console.log(s);e(".box").innerHTML=`
 <div class="img">
             <img src="${s[0].img}" alt="">
         </div>
@@ -10,4 +10,4 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("详情");const a=JSON.
         <div class="shop">
             <span>快递：0.00</span><span>月销量${s[0].sold}件</span><span>天津</span>
         </div>
-`;function e(n){return document.querySelector(n)}
+`;function e(a){return document.querySelector(a)}
