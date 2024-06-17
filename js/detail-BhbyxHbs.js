aad2369e9ab82fcb562f972d84543e7396595dcd
马璟 <2443065482@qq.com>
2024-06-17 11:41:49 +0800

    Updates

--- js/detail-BhbyxHbs.js
+++ js/detail-BhbyxHbs.js
@@ -1,13 +1,13 @@
 import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("详情");const n=JSON.parse(localStorage.getItem("id")),o=JSON.parse(localStorage.getItem("index"))*1;console.log(n,o);const s=n.filter(a=>a.item_id===o);console.log(s);e(".box").innerHTML=`
 <div class="img">
-            <img src="${s[0].img}" alt="">
+            <img src="${s.img}" alt="">
         </div>
-        <h2>￥${s[0].price} <span>促销价</span></h2>
+        <h2>￥${s.price} <span>促销价</span></h2>
         <p>价格<span>￥152</span> </p>
         <div class="main">
-        ${s[0].title}
+        ${s.title}
         </div>
         <div class="shop">
-            <span>快递：0.00</span><span>月销量${s[0].sold}件</span><span>天津</span>
+            <span>快递：0.00</span><span>月销量${s.sold}件</span><span>天津</span>
         </div>
 `;function e(a){return document.querySelector(a)}
