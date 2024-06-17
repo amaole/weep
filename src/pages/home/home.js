eb051ee64cf5f97b3fe20fdda612de033c3df43d
马璟 <2443065482@qq.com>
2024-06-17 11:18:38 +0800

    修改3

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -41,10 +41,12 @@ $(".deformation").addEventListener("click",(e)=>{
 document.addEventListener("click",(e)=>{
     if(e.target.nodeName==="DL"){
         const i=e.target.getAttribute("id")
-        console.dir(i)
+   
         localStorage.setItem("index",JSON.stringify(i))
-        const f=JSON.parse(localStorage.getItem("id")).slice(JSON.stringify(i),1)
-        console.log(f)
+        console.log(JSON.parse(JSON.stringify(i)))
+
+        // const f=JSON.parse(localStorage.getItem("id")).slice(JSON.stringify(i),1)
+        // console.log(f)
         location.assign("./detail.html")
 
     }
@@ -77,18 +79,21 @@ async function jumpPage(url) {
     // let price=res.data.items.price
     // console.log(price)
     $(".price").addEventListener("click",function(){
+        
         if (fals) {
             res.data.items.sort((a, b) => {
                 return a.price - b.price
             })
             readtable(res.data.items)
-           fals === false
+            fals = false
+           
         }else{
             res.data.items.sort((a, b) => {
                 return  b.price-a.price 
             })
             readtable(res.data.items)
-            fals ===true
+            fals = true
+            
         } 
         
         console.log(fals)
